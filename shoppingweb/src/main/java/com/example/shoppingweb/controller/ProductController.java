1198c7896f179c89ed64daa88370e99b2f1b5ccf
A05D730246 <152590426+A05D730246@users.noreply.github.com>
2024-03-18 23:18:18 +0800

    update
    update

--- shoppingweb/src/main/java/com/example/shoppingweb/controller/ProductController.java
+++ shoppingweb/src/main/java/com/example/shoppingweb/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.shoppingweb.model.*;
@@ -69,7 +70,7 @@ public class ProductController {
 	
 		 
 		//更新商品
-			@PostMapping("/products/update/{productId}")
+			@PutMapping("/products/update/{productId}")
 			public ResponseEntity<String> updateProduct(@PathVariable String productId, @RequestBody Product updatedProduct) {
 			    try {
 			        srv.updateProduct(productId, updatedProduct);
